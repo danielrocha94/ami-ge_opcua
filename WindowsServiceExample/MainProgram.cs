5f4451ad46c20e60e8412e90ac7ec2ce4a9274aa
danielrocha94 <danielrocha94@msn.com>
2017-09-25 13:12:31 -0500

    Inicializar repositorio con esqueleto

---
+++ WindowsServiceExample/MainProgram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsServiceExample
+{
+    static class MainProgram
+    {
+#if DEBUG
+        private static NotifyIcon _notifyIcon = new NotifyIcon();
+        private static ContextMenuStrip _menu = new ContextMenuStrip();
+
+        /// <summary>
+        /// The main entry point for the debug version
+        /// </summary>
+        [MTAThread]
+        static void Main(string[] args)
+        {
+            var service = new Service1();
+
+            service.Start();
+            
+            Application.Run();
+
+            service.Stop();
+        }
+#else
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+#endif
+    }
+}
