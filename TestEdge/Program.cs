b3409d58969efb529a17484fc5632eb2034d1fa7
danielrocha94 <danielrocha94@msn.com>
2017-09-27 08:50:28 -0500

    Agregar Edge y mover nodejs dentro de Edge

---
+++ TestEdge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EdgeJs;
+
+namespace TestEdge
+{
+    class Program
+    {
+        public static async Task Start()
+        {
+            string script = File.ReadAllText("TestServer.js");
+            var func = Edge.Func(script);
+
+            Console.WriteLine(await func(".NET"));
+
+        }
+
+        static void Main(string[] args)
+        {
+            Start().Wait();
+        }
+    }
+}
